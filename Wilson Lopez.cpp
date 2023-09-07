5ac1d5ed244a3ab85e05253f15ee8c82fef7c6b9
Wilson Lopez <wilson.alopez24@gmail.com>
2023-09-06 23:37:08 -0500

    Formatted code and added more comments.

--- Wilson Lopez.cpp
+++ Wilson Lopez.cpp
@@ -53,21 +53,19 @@ void StoreContents(contents& csvFile, std::string f, unsigned int& trading_days)
 	c.open(f);
 	
 	if (!c.is_open())
-	{
 		exit(0);
-	}
 	
 	if (c.is_open())
 	{
 		std::string first_line;
 		std::string temp1, temp2, temp3, temp4, temp5, temp6, temp7;
-		getline(c, first_line, '\n'); // "first_line" reads the first line in the .csv file.
+		getline(c, first_line, '\n');
 
 		// A while loop that repeats if c is not at the end of the file.
 		while (!c.eof())
 		{
 			getline(c, temp1, ',');					// Read until it hits a comma and store it inside temp1.
-			csvFile.date.push_back(temp1);			// Push back the value of temp into csvFile.date.
+			csvFile.date.push_back(temp1);			// Push back the value of temp1 into csvFile.date.
 			
 			getline(c, temp2, ',');					// Read until it hits a comma and store it inside temp2.
 			csvFile.open.push_back(stof(temp2));	// Push back the value inside of temp2 as float, into csvFile.open.
@@ -86,11 +84,12 @@ void StoreContents(contents& csvFile, std::string f, unsigned int& trading_days)
 
 			getline(c, temp7, '\n');				// Read until it hits a newline and store it inside temp7.
 			csvFile.volume.push_back(stoi(temp7));	// Push back the value of temp7 as an integer into csvFile.volume.
-			trading_days++;
+			
+			trading_days++;							// Increment the number of trading days.
 			
 		}
 
-		// Deletes the newline that is located at the last character
+		// Removes newline character from corresponding members of csvFile data structure.
 		csvFile.date.pop_back();				
 		csvFile.open.pop_back();
 		csvFile.high.pop_back();
@@ -109,24 +108,25 @@ void PercentChange(contents& csvFile, unsigned int t_days)
 {
 	for (unsigned int i = 0; i < t_days; i++)
 	{
-		csvFile.percent_change.push_back(i);
+		csvFile.percent_change.push_back(float(i));
 		csvFile.percent_change[i] = (csvFile.adj[i] - csvFile.open[i]) / csvFile.open[i] * 100;
 	}
 }
 
-// This function sorts the percent change values, as well as the dates, open and adj values.
+// This function sorts the percent change, date, open and adj values in descending order (based on the percent change order).
 void SortValues(contents& csvFile)
 {
 	bool swap;
 	std::string t1;
 	float temp, t2, t3;
-	int size_minus_one = (csvFile.percent_change.size() - 1);	// The size of the vector is subtracted by 1.
+	int size_minus_one = (int(csvFile.percent_change.size()) - 1);	// For traversing the vector inside the csvFile data structure.
 
 	do
 	{
 		swap = false;	// Assume that all values are in order.
 		for (int count = 0; count < size_minus_one; count++)
 		{
+			// Compares percent change values inside vector.
 			if (csvFile.percent_change[count] < csvFile.percent_change[count + 1])
 			{
 				// Sorts the Percent Change values that are inside the struct.
@@ -134,15 +134,18 @@ void SortValues(contents& csvFile)
 				csvFile.percent_change[count] = csvFile.percent_change[count + 1];
 				csvFile.percent_change[count + 1] = temp;
 				swap = true;
-				// Sorts the Dates inside the data structure, accordingly to the Percent Change order.
+
+				// Sorts the Dates inside the data structure, according to the Percent Change order.
 				t1 = csvFile.date[count];
 				csvFile.date[count] = csvFile.date[count + 1];
 				csvFile.date[count + 1] = t1;
-				// Sorts the Open values that are inside the struct, accordingly to the Percent Change order.
+
+				// Sorts the Open values that are inside the struct, according to the Percent Change order.
 				t2 = csvFile.open[count];
 				csvFile.open[count] = csvFile.open[count + 1];
 				csvFile.open[count + 1] = t2;
-				// Sorts the Adj. Close values that are inside the struct, accordingly to the Percent Change order.
+
+				// Sorts the Adj. Close values that are inside the struct, according to the Percent Change order.
 				t3 = csvFile.adj[count];
 				csvFile.adj[count] = csvFile.adj[count + 1];
 				csvFile.adj[count + 1] = t3;
@@ -154,11 +157,12 @@ void SortValues(contents& csvFile)
 int main()
 {
 	contents csvFile;
-	std::string filename, ticker_name;
-	unsigned int ticker_length, trading_days = 0, user_input;
+	std::string filename;
+	unsigned int trading_days = 0, user_input;
 
 	Compute_1_Filename(filename);
 
+	// Used to show filename without file extension.
 	//ticker_length = filename.length() - 4; // 4 is the length of ".csv"
 	//ticker_name = filename.substr(0, ticker_length);
 	
@@ -166,7 +170,7 @@ int main()
 	
 	trading_days--;	// Substracts a number from the trading days, as one of the rows inside the file are the column titles.
 
-	std::cout << "This program will analyze \"" << filename << "\"";	// Outputs the name of the ticker to the console screen.
+	std::cout << "This program will analyze \"" << filename << "\"";	// Outputs the name of the filename to the console screen.
 
 	std::cout << " from " << csvFile.date.front() << " to " << csvFile.date.back() << std::endl;	// Outputs the start and end date that was found in the file.
 
@@ -175,6 +179,9 @@ int main()
 	// Input the number of records from the console screen.
 	std::cout << "Enter the number of increase records you want to find: ";
 	std::cin >> user_input;
+
+	// Validates that number of records that user wants to find is within range.
+	// (Cannot be more than number of traiding days or less than zero.)
 	while (user_input > trading_days || user_input < 0)
 	{
 		std::cout << std::endl << "The file does not contain enough trading days." << std::endl;
@@ -183,6 +190,7 @@ int main()
 		std::cin >> user_input;
 	}
 
+	// Column heads for output values.
 	std::cout << std::endl << "Date" << "\t \t" << "Open" << "\t \t" << "A.Close" << " \t" << "Percent Change" << std::endl;
 
 	PercentChange(csvFile, trading_days);
